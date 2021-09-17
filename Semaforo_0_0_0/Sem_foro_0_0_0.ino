284794cb3420eb27a64496166ee7e5d2471c2e09
Andressa Padilha <72490924+andressapadilha@users.noreply.github.com>
2021-09-17 20:34:52 -0300

    Update Sem_foro_0_0_0.ino

--- Semaforo_0_0_0/Sem_foro_0_0_0.ino
+++ Semaforo_0_0_0/Sem_foro_0_0_0.ino
@@ -1,39 +1,3 @@
-/*
-  Semáforo_0_0_0
-
-  Semáforo de 3 vias:
-  *9 LEDS
-  - 3 Vermelhos
-  - 3 Amarelos
-  - 3 Verdes
-
-  *9 RESISTORES
-  - 270
-
-  Conexão Arduino
-
-  Semáforo 1
-   
-  Vermelho (13)   - Jumper Branco
-  Amarelo  (12)   - Jumper Vermelho
-  Verde    (11)   - Jumper Amarelo
-
-  Semáforo 2
-
-  Vermelho (10)   - Jumper Verde
-  Amarelo  (9)    - Jumper Azul
-  Verde    (8)    - Jumper Marrom
-
-  Semáforo 3
-
-  Vermelho (7)   - Jumper Roxo
-  Amarelo  (6)   - Jumper Verde
-  Verde    (5)   - Jumper Azul
-
-  Data 07/09/2021
-  Andressa Mikaela Padilha
-*/
-
  #define vmA 13
  #define amA 12
  #define vdA 11
